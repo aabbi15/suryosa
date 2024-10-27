fc5477cbe5cadada8a4a17ececfd28a695a8cf34
Abhishek Abbi <abhishekabbi.work@gmail.com>
2024-10-27 13:06:30 +0530

    solar3d responsive

--- nextjs/src/components/solar3d.js
+++ nextjs/src/components/solar3d.js
@@ -6,11 +6,15 @@ export default function Solar3D() {
         <div className=" rounded-[20px] bg1 bgslate-900 mx-[5%] shadow-lg py-10">
         
         <div className="grid grid-cols-2">
-            <div className="flex pl-[40px] flex-col items-center justify-center">
-                <h1 className="text-2xl md:text-5xl mb-10 font-bold  text-white title_underline self-start">Why Solar?</h1>
+            <div className="col-span-2 md:col-span-1 flex px-5 md:px-0 md:pl-[40px] flex-col items-center justify-center">
+
+
+                <h1 className="text-2xl md:text-5xl mb-10 font-bold  text-white title_underline md:self-start">Why Solar?</h1>
                 <SolarBenefitsComponent/>
             </div>
-            <div className="flex items-center justify-center">
+
+
+            <div className="hidden md:flex items-center justify-center">
             <Card3D/>
             </div>
         </div>
